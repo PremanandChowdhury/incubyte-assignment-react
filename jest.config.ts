e59a6b51e7cc3b40595d36d170183e2abbb68d26
Premanand Chowdhury <freudigprem@gmail.com>
2024-12-27 22:04:21 +0530

    hotfix: configuration fixed

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,16 @@
-import type {Config} from 'jest';
+import type { Config } from 'jest';
 
 const config: Config = {
   verbose: true,
+  preset: 'ts-jest', // Use ts-jest preset
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest', // Transform TypeScript files using ts-jest
+  },
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'], // Setup files to run before each test,
+  testEnvironment: 'jsdom', // Use jsdom as test environment
+  moduleNameMapper: {
+    '\\.css$': 'identity-obj-proxy',
+  },
 };
 
 export default config;
